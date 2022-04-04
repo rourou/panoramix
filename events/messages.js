6e6370ac0e44e4bfba4489a124a01fe3d12ebd06
romain <rouillard.romain@gmail.com>
2022-04-04 08:42:52 +0200

    deploy at start

--- events/messages.js
+++ events/messages.js
@@ -1,7 +1,19 @@
+const memberCoc = require(`../datas/membersCoc.json`)
+
 module.exports = {
     name: 'messageCreate',
     once: false,
     async execute(message) {
         console.log("message", message.content, " auteur: ", message.author.username, " ID: ", message.author.id, "channelId:", message.channel.id)
+
+        //recherche si on connais un village
+        const userDiscordFound = memberCoc.find((element) => element.userDiscord.id === message.author.id)
+
+        if (userDiscordFound) {
+            console.log('userDiscordFound:', !!userDiscordFound)
+        } else {
+            console.log('userDiscordFound:', !userDiscordFound)
+        }
+
     },
 };
