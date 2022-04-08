1ec9a4bd8d1ce32d8494c84d6911c80881e8d34e
romain <rouillard.romain@gmail.com>
2022-04-08 13:06:18 +0200

    fix

--- commands/lien.js
+++ commands/lien.js
@@ -67,7 +67,7 @@ module.exports = {
                 infosMember = await clash.getPlayer(tag)
 
                 if (infosMember.data !== "N/A") {
-                    console.log('interaction.user:', interaction.user)
+                    console.log('interaction.user:', interaction.user.username)
                     const add = await db.addUpdateUser({
                         db: "dayZero",
                         tag: `#${tag}`,
