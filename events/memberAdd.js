2bb6c6df55405db79ed1fb1472792a2fb0f0f024
romain <rouillard.romain@gmail.com>
2022-04-11 18:52:13 +0200

    fix

--- events/memberAdd.js
+++ events/memberAdd.js
@@ -30,6 +30,7 @@ module.exports = {
                 { name: `---`, value: ` ${bold("1 - ")}1000 points aux jeux tu feras\n${bold(" 2 -")} Ton statut de guerre à jour tu tiendras\n${bold(" 3 -")} Poli et courtois tu seras\n${bold(" 4 -")} En fonction de ton profil le bon clan tu choisira\n${bold(" 5 -")} A la vie du clan tu participeras\n${bold(" 6 -")} Dans les temps tu attaqueras\n${bold(" 7 -")} Le discord tu suivras\n${bold(" 8 -")} Des troupes tu donneras\n${bold(" 9 -")} Non préma tu es et resteras\n${bold("10 -")} Si non respect ton exclusion tu accepteras` },
                 { name: `---`, value: `Prends tes marques, amuse toi et n'hésites pas à poser des questions ` }
             )
+        console.log('embed:', embed)
         //avec le bouton pour accepter les regles
         const row = new MessageActionRow()
             .addComponents(
