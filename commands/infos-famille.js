ea0d8192e3fb72800133765ba5b07447738f3a38
rourou <rouillard.romain@gmail.com>
2022-05-29 15:42:30 +0200

    Update infos-famille.js

--- commands/infos-famille.js
+++ commands/infos-famille.js
@@ -11,7 +11,8 @@ module.exports = {
         const recup = {
             armee: await clash.getClan('2JQ82LG').then((res) => { return res.data }),
             asterix: await clash.getClan('YV902CU').then((res) => { return res.data }),
-            alpha: await clash.getClan('2LYU98Q2').then((res) => { return res.data })
+            alpha: await clash.getClan('2LYU98Q2').then((res) => { return res.data }),
+            walkingMonster: await clash.getClan('P0LLLVPR').then((res) => { return res.data })
         }
         const embed_1 = new MessageEmbed()
             .setColor('#ffffff')
@@ -19,7 +20,19 @@ module.exports = {
 
         const embed_2 = new MessageEmbed()
             .setColor('#ffffff')
-            .setTitle(`CLAN 1 : L'ARMÉE (${recup.armee.members}/50)`)
+            .setTitle(`CLAN 1 : WALKING MONSTER (${recup.walkingMonster.members}/50)`)
+            .setURL('https://link.clashofclans.com/fr?action=OpenClanProfile&tag=P0LLLVPR')
+            .setDescription('Clan école de la famille')
+            .setThumbnail(recup.walkingMonster.badgeUrls.medium)
+            .addFields(
+                { name: 'Tag ', value: '#P0LLLVPR' },
+                { name: 'Lien', value: 'https://link.clashofclans.com/fr?action=OpenClanProfile&tag=P0LLLVPR', inline: true },
+                { name: 'Description', value: recup.walkingMonster.description }
+            )
+
+        const embed_3 = new MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle(`CLAN 2 : L'ARMÉE (${recup.armee.members}/50)`)
             .setURL('https://link.clashofclans.com/fr?action=OpenClanProfile&tag=2JQ82LG')
             .setDescription('Clan chill / GDC organisée avec stratégie / accompagnement en stratégie et nouvelles compo')
             .setThumbnail(recup.armee.badgeUrls.medium)
@@ -28,9 +41,10 @@ module.exports = {
                 { name: 'Lien', value: 'https://link.clashofclans.com/fr?action=OpenClanProfile&tag=2JQ82LG', inline: true },
                 { name: 'Description', value: recup.armee.description }
             )
-        const embed_3 = new MessageEmbed()
+
+        const embed_4 = new MessageEmbed()
             .setColor('#ffffff')
-            .setTitle(`CLAN 2 : ASTERIX TEAM (${recup.asterix.members}/50)`)
+            .setTitle(`CLAN 3 : ASTERIX TEAM (${recup.asterix.members}/50)`)
             .setURL(' https://link.clashofclans.com/fr?action=OpenClanProfile&tag=YV902CU')
             .setDescription('CLAN chill / à la cool / rigolade / foutraque')
             .setThumbnail(recup.asterix.badgeUrls.medium)
@@ -40,6 +54,6 @@ module.exports = {
                 { name: 'Description', value: recup.asterix.description }
             )
 
-        await interaction.reply({ ephemeral: false, embeds: [embed_1, embed_2, embed_3] })
+        await interaction.reply({ ephemeral: false, embeds: [embed_1, embed_2, embed_3, embed_4] })
     },
 };
